9e6c9281c493e02b58ab96681155c7f3cd460259
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-04-04 17:25:15 +0430

    setup mongoose

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { json, urlencoded } from "body-parser"
 import cors from "cors"
 import express from "express"
 import helmet from "helmet"
+import mongoose from "mongoose"
 
 export default class App {
     constructor() {
@@ -11,21 +12,35 @@ export default class App {
 
     initialize() {
         this.setupExpress()
+        this.setupMongodb()
+        this.configuration()
     }
 
     setupExpress() {
         const server = createServer(this.app)
-        server.listen(config.port, () =>  console.log(`Server running on port ${config.port}`))
+        server.listen(config.port, () => console.log(`Server running on port ${config.port}`))
+    }
+
+    setupMongodb() {
+        mongoose.Promise = global.Promise
+        mongoose.connect("", {
+                useNewUrlParser: true
+            },
+            err => {
+                err ? console.log(err.message) : console.log("Database connected")
+            })
     }
 
     configuration() {
         this.app.use(helmet())
         this.app.use(cors({
-			credentials: true,
-			methods: "GET, POST, PUT, DELETE",
-			origin: "*"
-		}))
+            credentials: true,
+            methods: "GET, POST, PUT, DELETE",
+            origin: "*"
+        }))
         this.app.use(json())
-        this.app.use(urlencoded({ extended: true }))
+        this.app.use(urlencoded({
+            extended: true
+        }))
     }
 }
