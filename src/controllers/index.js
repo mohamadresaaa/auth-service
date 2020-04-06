9c998bc9ba025b721898e96e535552e58a401a72
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-04-06 18:03:27 +0430

    add dynamic controllers

---
+++ src/controllers/index.js
@@ -0,0 +1,17 @@
+const { readdirSync } = require("fs")
+const { resolve } = require("path")
+
+const controllers = {}
+
+// Read controllers from directory
+const files = readdirSync(resolve(__dirname)).filter((item) =>
+	!item.match("index.js") && !item.match("baseController.js"))
+
+console.log(files)
+
+// Importing to controllers
+for (const file of files) {
+	controllers[file.replace("Controller.js", "")] = require(`${resolve(__dirname, file)}`)
+}
+
+module.exports = controllers
