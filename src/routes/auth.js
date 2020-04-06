297b924152fe3261611b96c2f5199462502b8843
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-04-06 15:01:20 +0430

    using validator and register schema at router

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,15 @@
 const router = require("express").Router()
 
+// Middleware
+const validator = require("../middleware/validator")
+
+// Lib
+const { register } = require("../lib/validationSchema")
+
 router.get("/verifyCode/:code", (req, res) => res.send("verifyCode"))
 router.post("/login", (req, res) => res.send("login"))
 router.post("/passwordRecovery", (req, res) => res.send("passwordRecovery"))
-router.post("/register", (req, res) => res.send("register"))
+router.post("/register", validator(register), (req, res) => res.send("register"))
 router.post("/resetPassword", (req, res) => res.send("resetPassword"))
 
 module.exports = router
