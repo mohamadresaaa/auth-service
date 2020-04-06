31e45f26c6159081c0473fb052530a348cbbcf25
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-04-06 15:29:46 +0430

    add login schema and use it

--- src/lib/validationSchema.js
+++ src/lib/validationSchema.js
@@ -1,9 +1,14 @@
 const joi = require("@hapi/joi")
 
+const login = joi.object().keys({
+	email: joi.string().required(),
+	password: joi.string().required()
+})
+
 const register = joi.object().keys({
 	email: joi.string().email({ minDomainSegments: 2 }).required(),
 	password: joi.string().min(8).required(),
 	username: joi.string().required().regex(/^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,}$/).message("invalid username")
 })
 
-module.exports = { register }
+module.exports = { login, register  }
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,10 +4,10 @@ const router = require("express").Router()
 const validator = require("../middleware/validator")
 
 // Lib
-const { register } = require("../lib/validationSchema")
+const { login, register } = require("../lib/validationSchema")
 
 router.get("/verifyCode/:code", (req, res) => res.send("verifyCode"))
-router.post("/login", (req, res) => res.send("login"))
+router.post("/login", validator(login), (req, res) => res.send("login"))
 router.post("/passwordRecovery", (req, res) => res.send("passwordRecovery"))
 router.post("/register", validator(register), (req, res) => res.send("register"))
 router.post("/resetPassword", (req, res) => res.send("resetPassword"))
