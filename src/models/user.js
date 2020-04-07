eede625a259a822b0994c2027869d57e5a6740ef
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-04-07 14:36:54 +0430

    hashing password

--- src/models/user.js
+++ src/models/user.js
@@ -1,3 +1,4 @@
+const { compare, hash, genSaltSync } = require("bcrypt")
 const { ErrorMessage } = require("../lib/messages")
 const { Schema, model } = require("mongoose")
 
@@ -60,6 +61,18 @@ const userSchema = new Schema({
 userSchema.index({ email: 1 })
 userSchema.index({ username: 1 })
 userSchema.index({ createdAt: -1 })
+
+userSchema.pre("save", async function (next) {
+	try {
+		// If password modified, hashing password
+		if (this.isModified("password")) {
+			this.password = await hash(this.password, genSaltSync(10))
+			next()
+		}
+	} catch (err) {
+		next(err)
+	}
+})
 
 // Manage and prevent copy information from being imported email & username
 userSchema.post("save", function (error, doc, next) {
