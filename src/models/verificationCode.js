d4275e35e46465e4ae30b9cbe6e5c8589db9af1b
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-04-07 14:20:39 +0430

    add verificationCode schema

---
+++ src/models/verificationCode.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose")
+const { v5 } = require("uuid")
+
+const verificationCodeSchema = new Schema({
+	code: {
+		default: v5,
+		required: true,
+		type: String,
+		unique: true
+	},
+	expiryDate: {
+		required: true,
+		type: Date
+	},
+	for: {
+		required: true,
+		type: String
+	},
+	used: {
+		default: false,
+		type: Boolean
+	},
+	user: {
+		ref: "User",
+		required: true,
+		type: Schema.Types.ObjectId
+	}
+})
+
+// Index fields
+verificationCodeSchema.index({ code: 1 })
+verificationCodeSchema.index({ expiryDate: -1 })
+
+module.exports = model("VerificationCode", verificationCodeSchema)
