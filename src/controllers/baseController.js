204d281f2935a014665716102ba0cf9414af60c6
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-04-07 13:49:48 +0430

    set models to baseController

--- src/controllers/baseController.js
+++ src/controllers/baseController.js
@@ -1,10 +1,14 @@
 const autoBind = require("auto-bind")
 const services = require("../services")
+const models = require("../models")
 
 module.exports = class BaseController {
 	constructor () {
 		// Binding methods for using on child classes
 		autoBind(this)
+
+		// Set models
+		this[Symbol.for("models")] = models
 
 		// Set services
 		this[Symbol.for("services")] = services
