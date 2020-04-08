45a19bfedc5a3262bce4cc96cb1d8056f6083078
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-04-08 19:26:18 +0430

    use validator in account routes

--- src/routes/account.js
+++ src/routes/account.js
@@ -3,8 +3,14 @@ const router = require("express").Router()
 // Controllers
 const { account } = require("../controllers")
 
+// Middleware
+const validator = require("../middleware/validator")
+
+// Lib
+const { deactivation, reactivation } = require("../lib/validationSchema")
+
 router.get("/activation/:code", account.activation)
-router.post("/deactivation", account.deactivation)
-router.post("/reactivation", account.reactivation)
+router.post("/deactivation", validator(deactivation), account.deactivation)
+router.post("/reactivation", validator(reactivation), account.reactivation)
 
 module.exports = router
