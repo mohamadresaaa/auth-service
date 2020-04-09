1e636963f16e89d64cf4e97dd21de8f93d2c9ee1
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-04-09 21:30:38 +0430

    use authorization middleware in account router

--- src/routes/account.js
+++ src/routes/account.js
@@ -4,13 +4,14 @@ const router = require("express").Router()
 const { account } = require("../controllers")
 
 // Middleware
+const authorization = require("../middleware/authorization")
 const validator = require("../middleware/validator")
 
 // Lib
 const { deactivation, reactivation } = require("../lib/validationSchema")
 
 router.get("/activation/:code", account.activation)
-router.post("/deactivation", validator(deactivation), account.deactivation)
+router.post("/deactivation", authorization, validator(deactivation), account.deactivation)
 router.post("/reactivation", validator(reactivation), account.reactivation)
 
 module.exports = router
