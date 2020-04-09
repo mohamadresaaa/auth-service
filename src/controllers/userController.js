a0c08e35af42f087a5f9d8bcd2f691a70cd12aca
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-04-09 21:52:10 +0430

    use profile service to user controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,7 +3,7 @@ const baseController = require("./baseController")
 module.exports = new class UserController extends baseController {
 	async profile (req, res, next) {
 		try {
-
+			await this[Symbol.for("services")].user.profile(this, req.session, res)
 		} catch (error) {
 			next(error)
 		}
