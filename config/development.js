cb7666187cf3837d7178c4aee0d9f254ec89fe56
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-04-09 15:02:53 +0430

    add private and public key

--- config/development.js
+++ config/development.js
@@ -1,4 +1,6 @@
 module.exports = {
 	[Symbol.for("database url")]: "mongodb://127.0.0.1:27017/auth-service",
-	port: process.env.PORT || 3000
+	[Symbol.for("private key")]: process.env.PRIVATE_KEY || "privateKey",
+	[Symbol.for("public key")]: process.env.PUBLIC_KEY || "publicKey",
+	port: process.env.PORT || 3000,
 }
--- config/production.js
+++ config/production.js
@@ -1,4 +1,6 @@
 module.exports = {
 	[Symbol.for("database url")]: process.env.DATABASE_URL,
+	[Symbol.for("private key")]: process.env.PRIVATE_KEY,
+	[Symbol.for("public key")]: process.env.PUBLIC_KEY,
 	port: process.env.PORT || 4000
 }
