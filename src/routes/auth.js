320ba8e2f0097f3f8ea3da9b67bbec1bb69ece40
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-04-09 21:36:47 +0430

    add logout route in auth router

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,15 +4,17 @@ const router = require("express").Router()
 const { auth } = require("../controllers")
 
 // Middleware
+const authorization = require("../middleware/authorization")
 const validator = require("../middleware/validator")
 
 // Lib
 const { login, passwordRecovery, register, resetPassword } = require("../lib/validationSchema")
 
-router.get("/verifyCode/:code", auth.verifyCode)
 router.post("/login", validator(login), auth.login)
+router.get("/logout", authorization, auth.logout)
 router.post("/passwordRecovery", validator(passwordRecovery), auth.passwordRecovery)
 router.post("/register", validator(register), auth.register)
 router.post("/resetPassword", validator(resetPassword), auth.resetPassword)
+router.get("/verifyCode/:code", auth.verifyCode)
 
 module.exports = router
