d10bd16b102680fe1ea96734de35d5826348320e
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-04-09 22:20:19 +0430

    use validator and change password schema in user router

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,7 +6,10 @@ const { user } = require("../../controllers")
 // Middleware
 const validator = require("../middleware/validator")
 
+// Lib
+const { changePassword } = require("../lib/validationSchema")
+
 router.get("/", user.profile)
-router.post("/changePassword", user.changePassword)
+router.post("/changePassword", validator(changePassword), user.changePassword)
 
 module.exports = router
