3a06df9260cb751d293b59eb51ec7685f035cf8d
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-04-10 19:04:53 +0430

    add session controller

---
+++ src/controllers/sessionController.js
@@ -0,0 +1,19 @@
+const baseController = require("./baseController")
+
+module.exports = new class SessionController extends baseController {
+	async list (req, res, next) {
+		try {
+			
+		} catch (error) {
+			next(error)
+		}
+	}
+
+	async remove (req, res, next) {
+		try {
+		
+		} catch (error) {
+			next(error)
+		}
+	}
+}
