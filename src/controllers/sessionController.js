85dbf9db982409630b2216e494eb7a2795a5b2ca
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-04-10 19:20:34 +0430

    use sessions service in session controller

--- src/controllers/sessionController.js
+++ src/controllers/sessionController.js
@@ -3,7 +3,7 @@ const baseController = require("./baseController")
 module.exports = new class SessionController extends baseController {
 	async list (req, res, next) {
 		try {
-			
+			await this[Symbol.for("services")].session.list(this, req.session, res)
 		} catch (error) {
 			next(error)
 		}
@@ -11,7 +11,7 @@ module.exports = new class SessionController extends baseController {
 
 	async remove (req, res, next) {
 		try {
-		
+			await this[Symbol.for("services")].session.remove(this, req.params.id, res)
 		} catch (error) {
 			next(error)
 		}
